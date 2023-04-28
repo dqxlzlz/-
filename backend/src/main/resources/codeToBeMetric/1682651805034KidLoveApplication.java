d01be481d47bc98b29a601d217963c5be1ce896c
L <L@qq.com>
2023-04-28 12:05:14 +0800

    front update

---
+++ backend/src/main/resources/codeToBeMetric/1682651805034KidLoveApplication.java
@@ -0,0 +1,13 @@
+package com.kidlove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidLoveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KidLoveApplication.class, args);
+    }
+
+}
